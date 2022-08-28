3ace5e27755a1ed857ed84d36038a4821ca90b81
Matt <mjd014@live.co.uk>
2022-08-28 18:40:27 +0000

    added google maps marker clusterers.

--- assets/js/maps.js
+++ assets/js/maps.js
@@ -30,9 +30,7 @@ function initMap() {
         });
     });
 
-
-    const markerCluster = new markerClusterer.MarkerClusterer({
-        map,
-        markers
+    var markerCluster = new MarkerClusterer(map, markers, {
+        imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'
     });
 };
